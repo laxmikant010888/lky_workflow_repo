655a42251b2093148834a33a9ccf7607c9171591
laxmikant010888 <laxmikant010888@gmail.com>
2026-07-24 22:02:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ terraform {
 
 provider "azurerm" {
   features {}
-subscription_id = "a859689a-347e-41e4-8e6b-b4c1f510c9b6"
-  
 }
 
+
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-github-oidc-demo"
+  name     = "rg-selfhosted-demo"
   location = "Central India"
 }
+
 
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
