e6402bfb1837e21780b4082caf3cd819d9e63c4f
laxmikant010888 <laxmikant010888@gmail.com>
2026-08-11 00:55:37 +0530

    Delete ENV(parent_module) directory

--- ENV(parent_module)/DEV/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "dev-rg"
-        storage_account_name = "dev-lkstg"
-        container_name       = "dev-tfstate"
-        key                  = "terraform.tfstate"
-    }
-}
--- ENV(parent_module)/PROD/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "prod-rg"
-        storage_account_name = "prod-lkstg"
-        container_name       = "prod-tfstate"
-        key                  = "terraform.tfstate"
-    }
-}
